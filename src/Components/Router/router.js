924f38e445caa178f23f0994e4797cecf15a1d50
kubitre <kubitre@gmail.com>
2019-05-21 12:12:02 +0700

    upd

--- src/Components/Router/router.js
+++ src/Components/Router/router.js
@@ -5,13 +5,19 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainPage from '../../Containers/MainPage';
 import AuthPage from '../../Containers/AuthPage';
 import NotFoundPage from '../../Containers/NotFoundPage';
+import PrivateRoute from './private_route';
+import RegisterPage from '../../Containers/RegisterPage';
+import SettingPage from '../../Containers/SettingPage';
 
 export default function Router(props){
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={MainPage}/>
-                <Route path="/auth" component={AuthPage}/>
+                <PrivateRoute exact path="/" component={MainPage}/>
+                <PrivateRoute exact path="/settings" component={SettingPage}/>
+                
+                <Route path="/local/registration" component={RegisterPage}/>
+                <Route path="/local/login" component={AuthPage}/>
                 <Route component={NotFoundPage}/>
             </Switch>
         </BrowserRouter>
