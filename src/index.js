f167a0205fa01284579ec7cba830d12ff819d17b
kubitre <kubitre@gmail.com>
2019-05-21 11:43:42 +0700

    add redux creator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {BrowserRouter} from 'react-router-dom';
-
 import * as serviceWorker from './serviceWorker';
 
 import './mainStyles.scss';
 import Router from './router';
 
+import {Provider} from 'react-redux';
+import configureStore from './Store';
+
 ReactDOM.render(
-<BrowserRouter>
+<Provider store={configureStore()}>
     <Router />
-</BrowserRouter>
+</Provider>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
