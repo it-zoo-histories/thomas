10504e935e82cb3957baaab21739c1d99aae7e0d
kubitre <kubitre@gmail.com>
2019-05-24 22:26:21 +0700

    upd

--- src/Store/ReducerEntry/index.js
+++ src/Store/ReducerEntry/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 
 import API_authPageState from '../StoreParts/AuthPage/reducer';
-import MI_messagesPartState from '../StoreParts/Messages/reducer';
-import IPI_inputMessageState from '../StoreParts/InputBlockPart/reducer';
+import MI_messagesPartState from '../StoreParts/MessengerPage/Messages/reducer';
+import IPI_inputMessageState from '../StoreParts/MessengerPage/InputBlockPart/reducer';
 import UPI_userPartState from '../StoreParts/UserPart/reducer';
 
 export default combineReducers({
